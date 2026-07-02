88024f8b744ae81a0a6e4a475364fa417c885e20
keventst <kevenpub.thia@gmail.com>
2026-07-02 23:49:38 +0200

    trying to fix the getdates

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,4 @@
-// Dynamically display the current year in the footer copyright line
+
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 
-// Dynamically display the document's last modified date
 document.getElementById("lastModified").innerHTML = document.lastModified;
