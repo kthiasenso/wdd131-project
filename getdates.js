aa6e8c765145ceabac46a24a172e8cc1abc8b375
keventst <kevenpub.thia@gmail.com>
2026-07-02 23:43:38 +0200

    get dates

---
+++ getdates.js
@@ -0,0 +1,5 @@
+// Dynamically display the current year in the footer copyright line
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Dynamically display the document's last modified date
+document.getElementById("lastModified").innerHTML = document.lastModified;
